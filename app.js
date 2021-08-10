2a26c63caffa9adbe0a2aa2b1a5ea93aa469b355
T-Damer <dpancov64@gmail.com>
2021-08-10 10:20:15 +0300

    Trying to deploy to heroku

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const { google } = require('googleapis')
 
 const app = express()
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: true }))
+const router = express.Router()
+app.set('view engine', 'html')
+app.use(express.static(__dirname + '/public'))
+app.use('/', router)
+// app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
